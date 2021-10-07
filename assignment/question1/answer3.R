6c2bee337b5b4166e6271b9ca91ef4638079d3e3
kukaro <justkukaro@naver.com>
2021-10-07 11:07:21 +0900

    refactor:img

--- assignment/question1/answer3.R
+++ assignment/question1/answer3.R
@@ -7,5 +7,6 @@ df <- read.table(
 df <- t(df[, c(-1, -2)])
 colname <- c('생산연령인구', '고령인구', '청소년인구')
 colnames(df) <- colname
-matplot(df, type = "b", pch = 1, col = 1:3)
+year <- 2010:2030
+matplot(df, x = year, type = "b", pch = 1, col = 1:3)
 legend('topleft', legend = colname, pch = 1, col = 1:3)
